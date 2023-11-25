dd07f7562b04c97676d2ecba5c86bae57cba16e8
kdo_1999/mac <kdo_1999@naver.com>
2023-11-25 19:04:44 +0900

    로그인 기능 학습

---
+++ src/main/java/hello/login/web/login/LoginForm.java
@@ -0,0 +1,13 @@
+package hello.login.web.login;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginForm {
+    @NotBlank
+    private String loginId;
+
+    @NotBlank
+    private String password;
+}
